0224ccec57e3b079a4e5566c01d5244d3e13ccd2
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-13 23:13:07 -0300

    Ajustando landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Timeline from "@/components/Timeline";
 import Contact from "@/components/Contact";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="snap-y snap-mandatory overflow-y-scroll h-screen">
+      <Navbar />
       <Hero />
       <Projects />
       <Timeline />
